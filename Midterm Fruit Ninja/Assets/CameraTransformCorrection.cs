b974f6baffaf1ca80652e050c3d7f096cb8fde47
Siyu Yao <ysyyuune@gmail.com>
2019-10-08 10:21:03 -0600

    Camera script
    added camera script

---
+++ Midterm Fruit Ninja/Assets/CameraTransformCorrection.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransformCorrection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject mainCamera;
+    public GameObject cameraProxy;
+    public float lerpSpeed; // speed at which to interpolate between the proxy and main camera
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Calculate the difference in rotation between the proxy rotation and current main camera rotation
+        Quaternion correction = cameraProxy.transform.rotation * Quaternion.Inverse(mainCamera.transform.localRotation);
+        // Set the rotation value of this camera to be a portion of the offset, over time this will correct slowly
+        transform.rotation = Quaternion.Lerp(transform.rotation, correction, lerpSpeed);
+        // copy the transform position
+        transform.position = cameraProxy.transform.position;
+    }
+}
