1f01371358561e04f91d8df2c43df2cc859c136a
Siyu Yao <ysyyuune@gmail.com>
2019-10-31 11:24:23 -0600

    make sound when

---
+++ Midterm Fruit Ninja/Assets/AppleCut.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleCut: MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject apple;
+    public float maxDistance = 5;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+    void OnTriggerStay(Collider other)
+    {
+        // other object is close
+        if (Vector3.Distance(other.transform.position, this.transform.position) < maxDistance)
+        {
+            audioSource.Play();
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+}
